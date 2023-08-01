98144e8bcad52b2cd5fa50eff9165413485950d2
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-07-31 22:02:16 -0700

    update package json

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 // Constant
-const HOST = "localhost";
+const HOST = 'localhost';
 const PORT = 3000;
 const app = express();
 
 /* Middleware */
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/success', (req, res) => {
+	res.sendFile(path.join(__dirname, 'success.html'));
+});
+
 /* Routes */
-app.post("/form", (req, res) => {
+app.post('/form', (req, res) => {
+	const formData = req.body;
+	console.log(formData);
 	try {
 		const formData = req.body;
 		console.log(formData);
-		// Extract the data
-
-		res.redirect("/success");
+		res.redirect('/success');
 	} catch (error) {
-		res.status(500).json({ status: "error", message: error.message });
+		res.status(500).json({ status: 'error', message: error.message });
 	}
 });
 
-app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get("/success", (req, res) => {
-	res.sendFile(path.join(__dirname, "success.html"));
-});
-
-app.post("/json", (req, res) => {
+app.post('/json', (req, res) => {
 	try {
 		const formData = req.body;
 		console.log(formData);
-		res.json({ status: "success", message: "Data received" });
+		res.json({ status: 'success', message: 'Data received' });
 		//ajax request
 	} catch (error) {
-		res.status(500).json({ status: "error", message: error.message });
+		res.status(500).json({ status: 'error', message: error.message });
 	}
 });
 
